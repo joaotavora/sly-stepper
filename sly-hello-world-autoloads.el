355c94235afa9f79eefff1d22e97fcfa9c31d70c
R Primus <rprimus@users.noreply.github.com>
2019-07-01 15:43:12 +0100

    * sly-hello-world-autoloads.el: regenerate

--- sly-hello-world-autoloads.el
+++ sly-hello-world-autoloads.el
@@ -1,10 +1,32 @@
+;;; sly-hello-world-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+(add-to-list 'load-path (directory-file-name
+                         (or (file-name-directory #$) (car load-path))))
+
 
-;;;### (autoloads nil "sly-hello-world" "sly-hello-world.el"
-;;;;;;  (21964 37122 0 0))
+;;;### (autoloads nil "sly-hello-world" "sly-hello-world.el" (0 0
+;;;;;;  0 0))
 ;;; Generated autoloads from sly-hello-world.el
 
 (add-to-list 'sly-contribs 'sly-hello-world 'append)
 
-;;;***
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "sly-hello-world" '("sly-hello-world")))
 
-(provide 'sly-hello-world-autoloads)
+;;;***
+
+;;;### (autoloads nil "sly-hello-world-tests" "sly-hello-world-tests.el"
+;;;;;;  (0 0 0 0))
+;;; Generated autoloads from sly-hello-world-tests.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "sly-hello-world-tests" '("sly-hello-world-basic-test")))
+
+;;;***
+
+;; Local Variables:
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; sly-hello-world-autoloads.el ends here
